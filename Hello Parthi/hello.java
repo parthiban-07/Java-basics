f4b57ef8d66bbe147e6fdd63d3a15f37d3f6e6ca
Sriramkumar77 <sriramkumarbe.77@gmail.com>
2025-09-01 11:37:33 +0530

    Hello java

--- Hello Parthi/hello.java
+++
@@ -1,8 +0,0 @@
-    import java.lang.system ;
-    import java.util.Scanner;
-public class hello{
-     public static void main(String[] args) {
-       Scanner parthiban = new Scanner(System.in);
-       String a = parthiban.nextLine();
-       System.out.println(a);
-     }}

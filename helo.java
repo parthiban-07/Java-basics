dcdb81de3525511a19e79f3cf997ffefbc149304
parthiban-07 <parthibanm.ug.24.cs@scadengineering.ac.in>
2026-01-28 09:33:48 +0530

    updated feature

--- helo.java
+++ helo.java
@@ -1,10 +1,9 @@
-import java.lang.System;
 import java.util.Scanner;
 
 class helo {
     public static void main(String[] args) {
       Scanner parthi = new Scanner(System.in);
-      String a = parthi.nextLine();
+      int a = parthi.nextInt();
       int b = parthi.nextInt();
 
 

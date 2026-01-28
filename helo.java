efbd24be0a75691d204d61ddea503ecd1ce09739
parthiban-07 <parthibanm.ug.24.cs@scadengineering.ac.in>
2026-01-28 08:35:38 +0530

    initial commit

---
+++ helo.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class helo {
+    public static void main(String[] args) {
+      Scanner parthi = new Scanner(System.in);
+      String a = parthi.nextLine();
+      int b = parthi.nextInt();
+
+
+        System.out.println(a+b);
+    }
+}

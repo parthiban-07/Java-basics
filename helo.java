bcffc8d3f091275c193c4db4ffdbac416c9eda3c
parthiban-07 <parthibanm.ug.24.cs@scadengineering.ac.in>
2026-01-31 07:01:57 +0530

    Add Java programs

--- helo.java
+++ helo.java
@@ -4,9 +4,10 @@ class helo {
     public static void main(String[] args) {
       Scanner parthi = new Scanner(System.in);
       int a = parthi.nextInt();
-      int b = parthi.nextInt();
+      String b = parthi.nextLine();
 
 
-        System.out.println(a+b);
+        System.out.println("My name is" +a);
+        System.out.println("My age is" + b);
     }
 }

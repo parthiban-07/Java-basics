5eeb8fb37d209814861c4153b38d7fb4e3b11d5c
parthiban-07 <parthibanm.ug.24.cs@scadengineering.ac.in>
2026-02-05 07:05:10 +0530

    question 2

---
+++ q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class q2 {
+    public static void main(String[] args) {
+      Scanner Scan = new Scanner(System.in);
+      int a = Scan.nextInt();
+      int b = Scan.nextInt();
+      int c = Scan.nextInt();
+      int d = a*b*c;
+      int e = a+b+c;
+
+      
+      System.out.print(d/e);
+
+    }}

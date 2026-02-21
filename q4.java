5186c615aac91b94438584d791f6dd227d3e0e85
parthiban-07 <parthibanm.ug.24.cs@scadengineering.ac.in>
2026-02-21 18:06:18 +0530

    user input with if else condition

--- q4.java
+++ q4.java
@@ -3,13 +3,14 @@ class q4 {
     public static void main(String[] args) 
     {
       Scanner Scan = new Scanner(System.in);
-      String name = Scan.nextLine();
-      double Score =Scan.nextDouble();
-      Scan.nextLine();
-     String department = Scan.nextLine();
-        
-          System.out.println("My name is  "+name);
-          System.out.println("My Score is  "+Score/10 +"10");
-          System.out.println("My department is  "+department);
-
+      String Megana =Scan.nextLine();
+      
+      if (Megana.equals("Dead"))
+       {
+        System.out.print("Surya meets Ramya");
+      }
+      else
+      {
+        System.out.print("Surya weds Megana");
+      }
     }}

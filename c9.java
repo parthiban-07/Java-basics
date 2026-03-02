b56e84ea3a624da6273feca46a8ca2e8b2083d98
parthiban-07 <parthibanm.ug.24.cs@scadengineering.ac.in>
2026-03-02 20:16:45 +0530

    question no 9

---
+++ c9.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+class c9 {
+    public static void main(String []args){
+Scanner Scan = new Scanner(System.in);
+System.out.print("enter number 1");
+int a =Scan.nextInt();
+System.out.print("enter number 2");
+int b = Scan.nextInt();
+
+    for(int i=a;i<=b;i=i+1)
+    System.out.print(i);
+    }
+}
+        
